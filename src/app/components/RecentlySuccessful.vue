ea9922234da023e7a695facb45565f56ee92f2fd
Estéban <e.soubiran25@gmail.com>
2026-05-08 20:42:25 +0200

    feat: better quick news creation

---
+++ src/app/components/RecentlySuccessful.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+const recentlySuccessful = tv({
+  slots: {
+    base: 'text-muted text-sm',
+  },
+})
+
+export interface RecentlySuccessfulProps {
+  class?: any
+  ui?: Partial<typeof recentlySuccessful.slots>
+}
+export interface RecentlySuccessfulEmits {}
+export interface RecentlySuccessfulSlots {
+  default: () => any
+}
+</script>
+
+<script lang="ts" setup>
+const props = defineProps<RecentlySuccessfulProps>()
+defineEmits<RecentlySuccessfulEmits>()
+defineSlots<RecentlySuccessfulSlots>()
+
+const ui = computed(() => recentlySuccessful())
+</script>
+
+<template>
+  <span :class="ui.base({ class: [props.ui?.base, props.class] })">
+    <slot />
+  </span>
+</template>
